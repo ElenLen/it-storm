6ec8fd1b7fa1cc5e509b946471c5ad423c75ece9
ElenLen <relax-pl@yandex.ru>
2026-01-05 22:29:23 +0500

    feat: add page article

--- frontend/src/app/shared/services/articles.service.ts
+++ frontend/src/app/shared/services/articles.service.ts
@@ -4,6 +4,7 @@ import {Observable} from "rxjs";
 import {environment} from "../../../environments/environment";
 import {PopularArticlesType} from "../../../types/popular-articles.type";
 import {ActiveParamsType} from "../../../types/active-params.type";
+import {ArticleType} from "../../../types/article.type";
 
 @Injectable({
   providedIn: 'root'
@@ -12,16 +13,25 @@ export class ArticlesService {
 
   constructor(private http: HttpClient) { }
 
-  // запрос топ популярных услуг
+  // запрос топ популярных статей
   getPopularArticles(): Observable<PopularArticlesType[]> {
     return this.http.get<PopularArticlesType[]>(environment.api + "articles/top");
   }
 
-  // запрос всех услуг + параметры
+  // запрос всех статей + параметры
   getArticles(params: ActiveParamsType): Observable<{count: number, pages: number, items: PopularArticlesType[]}> {
     return this.http.get<{count: number, pages: number, items: PopularArticlesType[]}>(environment.api + "articles", {
       params: params
     });
   }
 
+  // запрос связанных статей по URL
+  getRelatedArticles(url: string): Observable<PopularArticlesType[]> {
+    return this.http.get<PopularArticlesType[]>(environment.api + "articles/related/" + url);
+  }
+
+  // запрос конкретной статьи по URL
+  getUrlArticle(url: string): Observable<ArticleType> {
+    return this.http.get<ArticleType>(environment.api + "articles/" + url);
+  }
 }
--- frontend/src/app/views/blog/detail/detail.component.ts
+++ frontend/src/app/views/blog/detail/detail.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {PopularArticlesType} from "../../../../types/popular-articles.type";
+import {ArticlesService} from "../../../shared/services/articles.service";
+import {ActivatedRoute} from "@angular/router";
+import {ArticleType} from "../../../../types/article.type";
+import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
+import {AuthService} from "../../../core/auth/auth.service";
+import {takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +14,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  // для хранения услуг
+  relatedArticles: PopularArticlesType[] = [];
+  // конкретная статья
+  urlArticle!: ArticleType;
+  // для безопасной загрузки HTML
+  sanitizedText!: SafeHtml;
+  // проверяем залогинен или нет
+  isLogged: boolean = false;
+
+  constructor(private articlesService: ArticlesService,
+              private activatedRoute: ActivatedRoute,
+              private sanitizer: DomSanitizer,
+              private authService: AuthService,) {
+    this.isLogged = this.authService.getIsLoggedId();
+  }
 
   ngOnInit(): void {
+  //   подписываемся на активейт парамс для взятия url
+    this.activatedRoute.params.subscribe(params => {
+      //   загружаем конкретную статью
+      this.articlesService.getUrlArticle(params['url'])
+        .subscribe((data: ArticleType) => {
+          this.urlArticle = data;
+          this.sanitizedText = this.sanitizeHtml(this.urlArticle.text);
+        })
+
+      //   загружаем связанные статьи
+      this.articlesService.getRelatedArticles(params['url'])
+        .subscribe((data: PopularArticlesType[]) => {
+          this.relatedArticles = data;
+        })
+    })
+
+    // Подписываемся на изменения статуса логина
+    this.authService.isLogged$
+      .subscribe((isLoggedIn: boolean) => {
+        this.isLogged = isLoggedIn;
+      });
+  }
+
+  sanitizeHtml(html: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(html);
   }
 
 }
